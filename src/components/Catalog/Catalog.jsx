0c87b905e146b28a6477e0ce10415dd0c4941c7c
Utavegu <utavegu@list.ru>
2021-03-16 16:46:48 +0500

    Реализовал черновой вариант загрузки товаров по категориям

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -26,40 +26,11 @@ function Preloader() {
 }
 
 export default function Catalog(props) {
-  // В принципе тут контекст не нужен - можно прямо сюда призывать 2 джейсон фетча... или нужен... давай пока оставь этот момент, пока до работы с корзиной не дошёл...
-  // const [, ,categories] = useContext(Context);
-
   const ALL_ITEMS_LINK = "http://localhost:7070/api/items";
-  const CATALOG_CATEGORIES_LINK = "http://localhost:7070/api/categories";
-  const linkToCategory = `http://localhost:7070/api/items?categoryId=X`;
-
+  
   const [items, setItems] = useState(null);
 	const [itemsError, setItemsError] = useState(null);
-
-  const [categories, setCategories] = useState(null);
-  const [categoriesError, setCategoriesError] = useState(null);
-
-  const [category, setCategory] = useState("Все");
-
   const [itemsUrl, setItemsUrl] = useState(ALL_ITEMS_LINK);
-
-  // Думаю, данные и стейт для категорий нужны только в категориях, а для элементов каталога - только в элементах каталога...
-  // Хотя... опираясь на эти данные я их отрисовываю... Но вообще это можно делать и внутри
-
-  const handleChangeCategory = (categoryName) => {
-    let link;
-    if (categoryName === "Все") {
-      link  = "http://localhost:7070/api/items"
-    } else {
-      let result = categories.find(category => category.title.toLowerCase() === categoryName.toLowerCase())
-      link = `http://localhost:7070/api/items?categoryId=${result.id}`;
-    }
-    setItemsUrl(link);
-  }
-
-
-  
-  let categoriesUrl = CATALOG_CATEGORIES_LINK;
 
 	useEffect(
 		() => {
@@ -83,27 +54,16 @@ export default function Catalog(props) {
 		[itemsUrl]
 	);
 
-  useEffect(
-		() => {
-			const fetchData = async () => {
-				try {
-					const response = await fetch(categoriesUrl);
-					if (!response.ok) {
-						throw new Error(response.statusText);
-					}
-					const data = await response.json();
-					setCategories(data);
-					setCategoriesError(null);
-				} 
-				catch (e) {
-					setCategoriesError(e);
-					console.dir(e.message);
-				} 
-			};
-			fetchData();
-		},
-		[categoriesUrl]
-	);
+  const handleChangeCategory = (categoryName, allCategories) => {
+    let link;
+    if (categoryName === "Все") {
+      link  = "http://localhost:7070/api/items";
+    } else {
+      let result = allCategories.find(category => category.title.toLowerCase() === categoryName.toLowerCase());
+      link = `http://localhost:7070/api/items?categoryId=${result.id}`;
+    }
+    setItemsUrl(link);
+  }
 
   let isCatalog;
   try {
@@ -112,32 +72,21 @@ export default function Catalog(props) {
     // Лучшее, до чего я додумался =) (чтобы поиск отображался только на экране каталога)
   }
 
+  /* 
+  1) Прелоадер и Ошибку в отдельные компоненты
+  */
   return (
     <section className="catalog">
       <h2 className="text-center">Каталог</h2>
 
       {isCatalog && <CatalogSearch />}
 
-      {(!categories) ? <Preloader /> :
-      <CatalogCategories 
-        onChangeCategory={handleChangeCategory} 
-        categories={categories} 
-      />}
-
-      {categoriesError && <div style={{color: "red", backgroundColor: "yellow", textAlign: "center", padding: 30, margin: 30, fontSize: 26, fontWeight: "bold"}}>Ошибка загрузки данных (категории каталога): {categoriesError.message}</div>}
-      {/* 
-      ВОТ ЭТУ БАТАРЕЮ НЕПЛОХО БЫ В УТИЛ УПИХАТЬ. КАК И ССЫЛКИ, ЕСЛИ С ЕНВ НЕ РАЗБЕРЕШЬСЯ 
-      */}
-
-      {/* Так, ещё одна проблема... эта работало до использования стэйта... нужно найти более универсальное решение... Походу опять привет промисам... Так, а что если данные изменять прямо в контексте?
-      */}
+      <CatalogCategories onChangeCategory={handleChangeCategory} />
 
       {(!items) ? <Preloader /> : <CatalogElements items={items} />}
 
       {itemsError && <div style={{color: "red", backgroundColor: "yellow", textAlign: "center", padding: 30, margin: 30, fontSize: 26, fontWeight: "bold"}}>Ошибка загрузки данных (товары каталога): {itemsError.message}</div>}
     
-
-
       <CatalogLoadMore />
       
     </section>
--- src/components/Catalog/CatalogCategories/CatalogCategories.jsx
+++ src/components/Catalog/CatalogCategories/CatalogCategories.jsx
@@ -1,26 +1,97 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 
-export default function CatalogCategories({categories, onChangeCategory: handleChangeCategory}) {
+// Тебя тоже в утилджээс
+function Preloader() {
+  return (
+    <div className="preloader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+
+export default function CatalogCategories({onChangeCategory: handleChangeCategory}) {
+
+  const CATALOG_CATEGORIES_LINK = "http://localhost:7070/api/categories";
+
+  const [categories, setCategories] = useState(null);
+  const [categoriesError, setCategoriesError] = useState(null);
 
   const handleClick = ({target}) => {
-    handleChangeCategory(target.textContent);
+    handleChangeCategory(target.textContent, categories);
   }
+
+  useEffect(
+		() => {
+			const fetchData = async () => {
+				try {
+					const response = await fetch(CATALOG_CATEGORIES_LINK);
+					if (!response.ok) {
+						throw new Error(response.statusText);
+					}
+					const data = await response.json();
+					setCategories(data);
+					setCategoriesError(null);
+				} 
+				catch (e) {
+					setCategoriesError(e);
+					console.dir(e.message);
+				} 
+			};
+			fetchData();
+		},
+		[CATALOG_CATEGORIES_LINK]
+	);
+
+  console.log();
+
+  /* 
+  1) В идеале НАВЛИНК, но с ними чото не получилось - видимо они должны быть внутри навигации или ещё что-то 
+  2) Чот как-то не очень отрисовка ВСЕ вручную... надо в массив сверху спереди просто ещё один объект запушить 
+  3) Прелоадер и компонент ошибки - в отдельные компоненты (с чилдреном)
+  4) Хреново конечно смотрится баррикада ниже и надо бы List-компонент ещё сделать, но пока так, пожалуй
+  */
   
   return (
-		<ul className="catalog-categories nav justify-content-center">
-      {/* Чот как-то не очень... надо в массив сверху спереди просто ещё один объект запушить */}
-      <li className="nav-item">
-				<Link onClick={handleClick} className="nav-link active" href="#">Все</Link>
-			</li>
+    <>
 
-      {/* В идеале НАВЛИНК, но с ними чото не получилось - видимо они должны быть внутри навигации или ещё что-то */}
+    {
+      (!categories)
+      ?
+      <Preloader />
+      : 
+      <ul className="catalog-categories nav justify-content-center">
 
-      {categories.map(category => 
-      <li className="nav-item" key={category.id}>
-				<Link onClick={handleClick} className="nav-link" href="#">{category.title}</Link>
-			</li>
-    )}
-    </ul>
+        <li className="nav-item">
+          <Link onClick={handleClick} className="nav-link active" href="#">Все</Link>
+        </li>
+
+        {categories.map(category => 
+        <li className="nav-item" key={category.id}>
+          <Link onClick={handleClick} className="nav-link" href="#">{category.title}</Link>
+        </li>
+      )}
+      </ul>
+    }
+
+    {
+      categoriesError 
+      && 
+      <div style={{
+        color: "red", 
+        backgroundColor: "yellow", 
+        textAlign: "center", 
+        padding: 30, 
+        margin: 30, 
+        fontSize: 26, 
+        fontWeight: "bold"
+        }}>Ошибка загрузки данных (категории каталога): {categoriesError.message}
+      </div>
+    }
+
+    </>
   )
 }
